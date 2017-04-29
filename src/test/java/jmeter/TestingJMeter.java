19b94a8796286561a2ad3d71eef6e15d3d9f2fc1
Juan Carlos <juancr82@gmail.com>
2017-04-29 12:12:13 -0500

    Unit test

---
+++ src/test/java/jmeter/TestingJMeter.java
@@ -0,0 +1,14 @@
+package jmeter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingJMeter {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
